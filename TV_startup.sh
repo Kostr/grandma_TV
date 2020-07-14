43571cb744f9426e30e9b6182e846aa5d46a9661
Kostr <aladyshev22@gmail.com>
2020-07-15 01:46:44 +0300

    Use unclutter to hide mouse cursor

--- TV_startup.sh
+++ TV_startup.sh
@@ -2,5 +2,6 @@
 
 echo "TV_startuppp" > /home/pi/my_startup_log.txt
 export DISPLAY=:0
+unclutter -idle 0 &
 #feh --fullscreen --hide-pointer /home/pi/grandma_TV/black_background.jpg > /home/pi/feh.log & 
 python3 /home/pi/grandma_TV/key_read.py /dev/input/event0 > /home/pi/mylog.txt 2> /home/pi/mylog.err 
